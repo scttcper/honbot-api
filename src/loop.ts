43a488f90f471bd1277899722609674a192a656a
Scott Cooper <scttcper@gmail.com>
2017-01-12 13:45:40 -0800

    should be working

--- src/loop.ts
+++ src/loop.ts
@@ -3,13 +3,14 @@ import mongo from './db';
 import { findNewest } from './scanner';
 
 async function loop() {
-  const newest = await findNewest();
   const db = await mongo;
+  const newest = await findNewest();
   let cur = 147503111;
-  while (cur <= newest.match_id) {
+  while (cur < newest.match_id) {
     const current = db.collection('matches').findOne({match_id: cur});
     cur++;
     if (!current) {
+      console.log(cur)
       const fail = { $set: { match_id: cur, failed: true }, $inc: { attempts: 1 } };
       await db.collection('matches').update({ match_id: cur }, fail, { upsert: true });
     }
