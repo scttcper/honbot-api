2c686da4edbd70672498a43dc304c9fb271f6342
Scott Cooper <scttcper@gmail.com>
2017-01-18 09:45:13 -0800

    add player matches endpoint, remove avatar

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import * as logger from 'koa-logger';
 import * as Router from 'koa-router';
 import * as koaRaven from 'koa2-raven';
 import * as Raven from 'raven';
-import * as request from 'request-promise-native';
 
 import config from '../config';
 import mongo from './db';
@@ -33,24 +32,6 @@ app.use((ctx, next) => {
 
 const router = new Router();
 
-router.get('/avatar/:accountId', async function avatar(ctx, next) {
-  const accountId = ctx.params.accountId;
-  ctx.assert(accountId, 400, 'No AccountId');
-  const url = `https://www.heroesofnewerth.com/getAvatar_SSL.php?id=${accountId}`;
-  const opt = {
-    uri: url,
-    method: 'HEAD',
-    resolveWithFullResponse: true,
-  };
-  try {
-    const res = await request(opt);
-    ctx.body = res.request.uri.href;
-  } catch (e) {
-    ctx.body = `https://s3.amazonaws.com/naeu-icb2/icons/default/account/default.png`;
-  }
-  return next();
-});
-
 router.get('/season/:nickname', async function getPlayer(ctx, next) {
   const lower = ctx.params.nickname.toLowerCase();
   const query = {
@@ -59,6 +40,14 @@ router.get('/season/:nickname', async function getPlayer(ctx, next) {
     'setup.nl': 1,
     'setup.officl': 1,
   };
+  const db = await mongo;
+  ctx.body = await db.collection('matches').find(query).sort({ match_id: -1 }).toArray();
+  return next();
+});
+
+router.get('/playerMatches/:nickname', async function getPlayer(ctx, next) {
+  const lower = ctx.params.nickname.toLowerCase();
+  const query = { 'players.lowercaseNickname': lower };
   const db = await mongo;
   ctx.body = await db.collection('matches').find(query).sort({ match_id: -1 }).toArray();
   return next();
