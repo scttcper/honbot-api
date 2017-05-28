c49c27709d9edc1c2467fe752ea3866a30e12774
Scott Cooper <scttcper@gmail.com>
2017-05-28 10:57:03 -0700

    remove database size

--- src/stats.ts
+++ src/stats.ts
@@ -12,22 +12,7 @@ export default async function stats() {
   const matches = await Matches.count();
   const lastDayDate = moment().subtract(1, 'days').subtract(140, 'minutes').toDate();
   const loadedLastDay = await Matches.count({ where: { createdAt: { $gt: lastDayDate } } });
-  const lastDay = await Matches.count({ where: { date: { $gt: lastDayDate } } });
-  const disksize = await sequelize.query(`
-  SELECT
-    CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
-        THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname))
-        ELSE 'No Access'
-    END AS SIZE
-  FROM pg_catalog.pg_database d
-    WHERE d.datname = '${config.database}'
-    ORDER BY
-    CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
-        THEN pg_catalog.pg_database_size(d.datname)
-        ELSE NULL
-    END DESC -- nulls first
-    LIMIT 20`).then((s) => s[0][0].size);
-  const res = { matches, lastDay, disksize, loadedLastDay };
+  const res = { matches, loadedLastDay };
   client.setex('stats:cache', 600, JSON.stringify(res));
   return res;
 }
