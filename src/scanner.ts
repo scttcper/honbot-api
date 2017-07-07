1d5a919799ff8cb0070153c1c60b40f06c27e689
Scott Cooper <scttcper@gmail.com>
2017-07-07 16:45:10 -0700

    call again instead of while loop

--- src/scanner.ts
+++ src/scanner.ts
@@ -20,57 +20,61 @@ const BATCH_SIZE = 25;
 let notExit = true;
 
 async function findNewMatches() {
-  let last = 0;
-  while (notExit) {
-    const [newestMatchId, newestMatchDate, diff] = await findNewest().catch((err) => {
-      sentry.captureException(err);
-    });
-    if (diff > 25) {
-      await sleep(60000, 'made no forward progress');
-    }
-    if (newestMatchId) {
-      const minutes = differenceInMinutes(new Date(), new Date(newestMatchDate));
-      const hours = Math.round(minutes / 60);
-      log(`Newest: ${newestMatchId} - Age: ${hours} hours`);
-      if (minutes < 180) {
-        await sleep(60000, 'matches too recent');
-        continue;
+  const [newestMatchId, newestMatchDate, diff] = await findNewest().catch((err) => {
+    sentry.captureException(err);
+  });
+  if (diff > 25) {
+    await sleep(60000, 'made no forward progress');
+  }
+  if (newestMatchId) {
+    const minutes = differenceInMinutes(new Date(), new Date(newestMatchDate));
+    const hours = Math.round(minutes / 60);
+    log(`Newest: ${newestMatchId} - Age: ${hours} hours`);
+    if (minutes < 180) {
+      await sleep(60000, 'matches too recent');
+      if (notExit) {
+        return findNewMatches().catch((e) => catchError(e));
       }
     }
-    const newest = newestMatchId ? newestMatchId : STARTING_MATCH_ID;
-    const matchIds = _.range(newest + 1, newest + BATCH_SIZE).map(String);
-    log('Finding new matches!');
-    await grabAndSave(matchIds, true);
-    last = newest;
-    await sleep(3000, 'findNewMatches sleep');
+  }
+  const newest = newestMatchId ? newestMatchId : STARTING_MATCH_ID;
+  const matchIds = _.range(newest + 1, newest + BATCH_SIZE).map(String);
+  log('Finding new matches!');
+  await grabAndSave(matchIds, true);
+  await sleep(3000, 'findNewMatches sleep');
+  if (notExit) {
+    return findNewMatches().catch((e) => catchError(e));
   }
 }
 
 async function findAllMissing() {
   let cur = 0;
   const hourAgo = subHours(new Date(), 1);
-  while (notExit) {
-    const missing = await Failed
-      .findAll({
-        where: {
-          id: { $gt: cur },
-          attempts: { $lt: 5 },
-          updatedAt: { $lt: hourAgo },
-        },
-        limit: 25,
-        order: [['id']],
-      });
-    if (!missing.length) {
-      // wait 30 minutes
-      await sleep(1800000, 'no missing found, reset cursor');
-      cur = 0;
-      continue;
+  const missing = await Failed
+    .findAll({
+      where: {
+        id: { $gt: cur },
+        attempts: { $lt: 5 },
+        updatedAt: { $lt: hourAgo },
+      },
+      limit: 25,
+      order: [['id']],
+    });
+  if (!missing.length) {
+    // wait 30 minutes
+    await sleep(1800000, 'no missing found, reset cursor');
+    cur = 0;
+    if (notExit) {
+      return findAllMissing().catch((e) => catchError(e));
     }
-    const missingIds = missing.map(x => x.toJSON().id);
-    log('Attempting old matches!');
-    await grabAndSave(missingIds, false);
-    cur = missingIds[missingIds.length - 1];
-    await sleep(10000, 'findAllMissing sleeping');
+  }
+  const missingIds = missing.map(x => x.toJSON().id);
+  log('Attempting old matches!');
+  await grabAndSave(missingIds, false);
+  cur = missingIds[missingIds.length - 1];
+  await sleep(10000, 'findAllMissing sleeping');
+  if (notExit) {
+    return findAllMissing().catch((e) => catchError(e));
   }
 }
 
@@ -82,7 +86,12 @@ process.on('SIGINT', () => {
   }, 2000);
 });
 
+function catchError(err: Error) {
+  log(err);
+  sentry.captureException(err);
+}
+
 if (!module.parent) {
-  findNewMatches();
-  findAllMissing();
+  findNewMatches().catch((e) => catchError(e));
+  findAllMissing().catch((e) => catchError(e));
 }
