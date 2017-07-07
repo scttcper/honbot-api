0e38a2fe4c50b1891e78366b6860471c518fa044
Scott Cooper <scttcper@gmail.com>
2017-07-07 12:28:50 -0700

    update packages

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,11 +1,11 @@
 import * as debug from 'debug';
-import { distanceInWordsToNow, addMilliseconds } from 'date-fns';
+import { formatDistance, addMilliseconds } from 'date-fns';
 
 const log = debug('honbot');
 
 export default function sleep(duration: number, reason: string = 'waiting') {
   return new Promise((resolve, reject) => {
-    const time = distanceInWordsToNow(addMilliseconds(new Date(), duration));
+    const time = formatDistance(addMilliseconds(new Date(), duration), new Date());
     log(`Sleeping for ${time} because ${reason}`);
     setTimeout(() => { resolve(0); }, duration);
   });
