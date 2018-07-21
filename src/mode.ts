8c64371e441535b4923eeb8e77ea2a67a10fcf74
Scott Cooper <scttcper@gmail.com>
2018-07-20 18:26:12 -0700

    add season 7

--- src/mode.ts
+++ src/mode.ts
@@ -20,6 +20,9 @@ export function getMode(match: any): string {
       version = version.replace(/(\.[0-9]+)$/, '');
     }
     // seasons
+    if (semver.satisfies(version, '>=4.5.0')) {
+      return 'Season 7';
+    }
     if (semver.satisfies(version, '>=4.4.0')) {
       return 'Season 6';
     }
